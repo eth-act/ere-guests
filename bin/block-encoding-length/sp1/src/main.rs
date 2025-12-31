fdd348e71ae5a926ce74f7a54037f5266a2a9849
han0110 <tinghan0110@gmail.com>
2025-12-31 01:48:52 +0000

    feat: update `block-encoding-length`

--- bin/block-encoding-length/sp1/src/main.rs
+++ bin/block-encoding-length/sp1/src/main.rs
@@ -2,7 +2,7 @@
 
 #![no_main]
 
-use block_encoding_length_guest::guest::{BlockEncodingLengthGuest, Guest};
+use block_encoding_length::guest::{BlockEncodingLengthGuest, Guest};
 use ere_platform_sp1::{SP1Platform, sp1_zkvm};
 
 sp1_zkvm::entrypoint!(main);
