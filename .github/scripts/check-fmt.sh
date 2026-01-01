157d04a29e274aae1b5d0074fded41a3a7f1b73f
han0110 <tinghan0110@gmail.com>
2026-01-01 05:05:22 +0000

    ci: simplify

--- .github/scripts/check-fmt.sh
+++ .github/scripts/check-fmt.sh
@@ -8,13 +8,11 @@ echo "Checking format of workspace..."
 echo ""
 cargo +nightly fmt --check --all || HAS_ERRORS=1
 
-CONFIG_PATH=$(realpath ./rustfmt.toml)
-
 while read -r CARGO_TOML; do
     DIR=$(dirname "$CARGO_TOML")
     echo "Checking format of $DIR..."
     echo ""
-    (cd "$DIR" && cargo +nightly fmt --check --all -- --config-path "$CONFIG_PATH") || HAS_ERRORS=1
+    (cd "$DIR" && cargo +nightly fmt --check --all) || HAS_ERRORS=1
 done < <(find bin -name "Cargo.toml" -type f)
 
 exit $HAS_ERRORS
