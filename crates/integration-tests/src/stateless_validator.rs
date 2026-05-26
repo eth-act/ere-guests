a4fe93a2c7fd6da193ab514c0ae42d8f110a7d19
han0110 <tinghan0110@gmail.com>
2026-05-26 13:20:07 +0000

    feat: revert test fixtures root

--- crates/integration-tests/src/stateless_validator.rs
+++ crates/integration-tests/src/stateless_validator.rs
@@ -37,47 +37,47 @@ fn expected_execution_payload_tree_roots() -> HashMap<B256, B256> {
     HashMap::from([
         (
             b256!("e4bd1c4dc22a58a0a9a8e789e2c54b4ace2d1ebc16a605c3976723b52fc011f1"),
-            b256!("63477d4b2d525534352ea13c86189340fcb6a51567c8a3a850220b549102e2d5"),
+            b256!("45328434f812b65daa21b4e8a3d6440d0da95fbd95a6c10b0a28f081cab53bd5"),
         ),
         (
             b256!("88c74fb93052f3bccc20e2ce709a97a6bd669cbdf1c3a54997b6f5cfda03accc"),
-            b256!("7af2f7a50e5f37f8292ed679a760264706cb0caafd57111398055bea41fbeaeb"),
+            b256!("f8ac723efd5d8d12af604948d0f46e53be47d18a23e6ac2adfbe3818ac1d0d98"),
         ),
         (
             b256!("77bd26b8aed0d14e1d78c180196de399840ee7462cb3be6f20981f63284b0bb7"),
-            b256!("6f8fde1fc6d437b3088a2d40a40d7bc673ab15bbdbbf3f72bca894856bc9126a"),
+            b256!("22de93d4f2bfcc6d35530d8ef3f57d60daec797a646a7ac66fbef1c240359f3d"),
         ),
         (
             b256!("4ea1ba2443afd99b07534559feb2d57c390489c2594c2a053e79ff066851db63"),
-            b256!("0100fc3604f2bb16343598faa4bbb307c7572b97e55f8b563003e70d1cb3ee68"),
+            b256!("2d4f61867176c82bab2c396559320b337b51412cfa8fbf988560b81f8ef4863c"),
         ),
         (
             b256!("e2154b8a9ad9fdc0e6e23dbd6be53b568cfe15da66d945272bd627ab488bc7fc"),
-            b256!("70ddf6f74e2faf7bab6459eb7e32535ec1a3c226bd02610644f8f1e5cefef846"),
+            b256!("66063b993a271908b668380e0e5ae2f915ccd993fd999bc75a7fc6e1cc5af9db"),
         ),
         (
             b256!("18e16e8807c5d723cfa8c1146474c63e0e334abedeb7bf4b1c3dfc44860d2db6"),
-            b256!("2b54f99dc26a518209641603ad50d9e100114359e0dfa62a4611e2930639ec87"),
+            b256!("760580ec7bb1dcc3b0a53fe0fec0f9dd94ab5c068b0e3c01128d11da7b5e679e"),
         ),
         (
             b256!("0853345a27fcf3de0d8f77be6465ccd638ab6fceff4f1aa95b2c8e3a20a94843"),
-            b256!("d20afc195c1439086fef74cc2406c026fb44db77d7a61d5e6fbac6b2f205b0b0"),
+            b256!("3694ce5950dfbc84bf1ab6d3d54b52c269761e3e74420500a1c2e4a3e74efcda"),
         ),
         (
             b256!("f93cbd9836c96bc35a5f3e97fcd5887fa4cf4fead6d5b5fb796df34b76506d14"),
-            b256!("a884be1af18cd8f213f6fa6db255b7281b647b5c8fd4ce984bf97bdc2ef26fa8"),
+            b256!("ecb22240d38111f6236483b84cda9f96cff8a9b8c8ecb5cc3df89c396a030104"),
         ),
         (
             b256!("91ff4738a9ca92dc46ad86cfad7d6e31f678553d965357180537f4653bb48d9b"),
-            b256!("95c63b0bd353b902db7138db05febca7ebd1ba112b4b581702f28128a0db5c57"),
+            b256!("c3c81e368dbbd449865caf4ef7721e19d6aa35b1b1480b58c4e412bf122f2558"),
         ),
         (
             b256!("2c576b118b1919886188c4a3b1f71143ca8b8a5c1814d8908beb004a6acb9fa1"),
-            b256!("26a3e9f1a689222cf9e7f136fd7f7ebaaa712896fe726f9b1a953ad4b67731af"),
+            b256!("f490f0d4368609cf81fd90e04da0d9a1fe7cba9e3e21fc77e01968386affdc90"),
         ),
         (
             b256!("53dcdbca6be3f94cd96a8f37900082d04850bfc5a285e9a88b2498f318c89809"),
-            b256!("d2f14474f7d3c364abc572439d90230d1f13e4d444b8612711bfa715ea3d9a8d"),
+            b256!("359e5dee47560e9cffc8947ee8d75c2a5d15c621ec2c647426c95e4b69275c7f"),
         ),
     ])
 }
