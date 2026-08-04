0977120cf3635914fe140448a38d3608fdc9d82b
han0110 <tinghan0110@gmail.com>
2026-08-04 03:28:41 +0000

    feat: bump ere to 7e8e2ba1001f7776547f0576e4eb49bd0155e0d2 and reth to f804dc1eaa06927b1bdf17f2b67572f6f6b2deee without needing reth patch

--- crates/stateless-validator-test/tests/zkvm_execution.rs
+++ crates/stateless-validator-test/tests/zkvm_execution.rs
@@ -68,17 +68,17 @@ declare_test!(Reth, OpenVM, RpcBpo2);
 // Reth divergences (in-block created-code resolution from EIP-8025).
 declare_test!(Reth, OpenVM, RpcGlamsterdamDevnet7, failures = 1);
 // Reth divergences.
-declare_test!(Reth, OpenVM, EestGlamsterdamDevnet7, failures = 17);
+declare_test!(Reth, OpenVM, EestGlamsterdamDevnet7, failures = 13);
 declare_test!(Reth, SP1, RpcBpo2);
 // Reth divergences (in-block created-code resolution from EIP-8025).
 declare_test!(Reth, SP1, RpcGlamsterdamDevnet7, failures = 1);
 // Reth divergences.
-declare_test!(Reth, SP1, EestGlamsterdamDevnet7, failures = 17);
+declare_test!(Reth, SP1, EestGlamsterdamDevnet7, failures = 13);
 declare_test!(Reth, Zisk, RpcBpo2);
 // Reth divergences (in-block created-code resolution from EIP-8025).
 declare_test!(Reth, Zisk, RpcGlamsterdamDevnet7, failures = 1);
 // Reth divergences + ZisK `zkvm-interface` impl bug.
-declare_test!(Reth, Zisk, EestGlamsterdamDevnet7, failures = 39);
+declare_test!(Reth, Zisk, EestGlamsterdamDevnet7, failures = 35);
 
 // Zesu
 
